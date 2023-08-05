4d6d21fc0746d4e093ab5a223a19c7c6c6232a09
Curtis Dev K <curt_king@coolsite.net>
2023-08-05 03:00:34 +0100

    Have added some responsive styling to the header and also wrapper for the new VoiceServices component. Removed background color on navbar, and added visual touches and animation to the header image

--- src/components/VoiceServices.js
+++ src/components/VoiceServices.js
@@ -1,9 +1,32 @@
 import './VoiceServices.css';
 
-export default function VoiceServices() {
+export default function VoiceServices({ microphone }) {   // pass in microphone image from parent component
     return (
         <div className="servicesWrapper">
-            <h3>Services I can Offer:</h3>
+            <h3>My Voice Over services include</h3>
+            <div className='services-list-wrap'>
+                <ul>
+                    <li>Animation</li>
+                    <li>Character</li>
+                    <li>eLearning</li>
+                    <li>Animation</li>
+                </ul>
+                <ul>
+                    <li>Explainer Videos</li>
+                    <li>YouTube</li>
+                    <li>Narration</li>
+                    <li>Video Games</li>
+                </ul>
+                <div className='samples-link'>
+                    <img className='samples-image' src={ microphone } title='Photo by Daniel Robert Dinu unsplash.com' alt="picture of a microhone which is used as a link to the voice samples page of website"></img>
+                    <a href="#" className='sample-anchor'>
+                        <button type='button'>Listen to some samples</button> 
+                    </a>
+                </div>
+            </div>
         </div>
     );
 }
+
+
+  
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,7 @@
 import './Home.css';
 import mediumImage from '../../assets/pictures/pexels-moose-photos-1037992.jpg';
 import smallImage from '../../assets/pictures/pexels-small-moose-photos-1037992.jpg';
+import microphone from '../../assets/pictures/microphone-removebg-preview.png';
 import { useEffect, useState } from 'react';
 import VoiceServices from '../../components/VoiceServices';
 
@@ -16,18 +17,19 @@ function Home() {
     }, []);
 
     return (
+        
         <div className="wrapper-home">
             <header>
                 { windowWidth > 500 ? (
-                <img className='headerImage' src={mediumImage} alt='You aint seeing shit' />
+                <img className='headerImage' src={mediumImage} alt='A set of headphones on a 50 50 split pink and light blue background' />
                 ) : (
-                <img className='headerImage' src={smallImage} alt='You aint seeing shit' />
+                <img className='headerImage' src={smallImage} alt='A set of headphones on a 50 50 split pink and light blue background' />
                 )}
                 <h1>Curtis King</h1>
                 <hr></hr>
                 <h2>VOICE OVER ACTOR</h2>
             </header>
-            <VoiceServices />
+            <VoiceServices microphone={microphone}/>
         </div>
     );
 }
